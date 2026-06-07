003c256df6e4f8465fae7fbf228a897a3874e76d
YoungGong <reggie01@naver.com>
2026-06-07 17:13:02 +0900

    change: directory

--- System/RandomSeed.cs
+++ System/RandomSeed.cs
@@ -1,8 +1,10 @@
 using System.Collections.Generic;
 
+// 스테이지에 같은 랜덤이 뜨게 하기 위한, 시드 고정형 랜덤 호출기
+
 public static class RandomSeed
 {
-    public static int Seed {get; private set;}= 0;
+    public static int Seed { get; private set; } = 0;
 
     public static void SetSeed(int seed)
     {
@@ -14,6 +16,7 @@ public static class RandomSeed
         System.Random rand = new System.Random(seed ?? Seed);
         return rand.Next(min, max);
     }
+
     public static List<T> Shuffle<T>(List<T> list, int? seed = null)
     {
         System.Random rand = new System.Random(seed ?? Seed);
